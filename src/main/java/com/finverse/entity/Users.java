c564a65ee688baa5234c18f685ce9ca4ff2054da
SathishChekuri <chekurisathish41@gmail.com>
2026-07-16 20:51:28 +0530

    Update User entity and project structure

---
+++ src/main/java/com/finverse/entity/Users.java
@@ -0,0 +1,60 @@
+package com.finverse.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.finverse.enums.AccountStatus;
+import com.finverse.enums.Gender;
+import com.finverse.enums.Role;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private String mobileNumber;
+
+    private LocalDate dateOfBirth;
+
+    @Column(length = 500)
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private AccountStatus status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
